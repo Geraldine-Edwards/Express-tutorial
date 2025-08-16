9818d6cb7fefd0bd0f7879f737f11206e325d0d4
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-08-16 15:00:16 +0100

    fetching and getting a data

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const app = express();
 app.get('/Home',(req,res)=>{
     res.send("Hello World!")
 })
+app.get('/Home/data',(req,res)=>{
+    fetch("https://zenquotes.io/api/random")
+    .then((response)=>{
+        const Response = response.json()
+        return Response
+    }).then((data)=>{
+         const Data = data[0]
+           res.send(Data)
+    }).catch((err)=>{
+        console.log("the Error is",err)
+    })
+  
+})
+
 
 app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
